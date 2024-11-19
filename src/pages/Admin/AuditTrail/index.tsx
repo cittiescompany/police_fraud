f8c18753c74a1396e27d1ac099b67fbcde2f1e3e
Adio-Damilare <adiodamilare44@gmail.com>
2024-11-19 23:19:17 +0100

    new commigt

--- src/pages/Admin/AuditTrail/index.tsx
+++ src/pages/Admin/AuditTrail/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Table from "./Table";
-
+import { adminUrl } from "../../../BackendUrl";
+import { useGetData } from "../../../content";
 const AuditTrail = () => {
+  const [data]=useGetData(`${adminUrl}user/audit/all`)
   return (
     <div>
       <h3>AuditTrail</h3>
-      <Table data={[]} />
+      <Table data={ data&&data.data} />
     </div>
   );
 };
--- src/pages/Bank/PostNoDebit/CourtOrder.tsx
+++ src/pages/Bank/PostNoDebit/CourtOrder.tsx
@@ -133,4 +133,4 @@ const HamburgerMenu = ({ update, data }: any) => {
     </div>
   );
 };
-export default CouterOrder;
+export default CouterOrder; 
